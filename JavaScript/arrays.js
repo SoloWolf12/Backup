df4ad40e9e55b58969fcb10714711450fe41fa3f
SoloWolf12 <danielaguerreberry@live.com.ar>
2021-09-29 20:42:54 -0300

    new

---
+++ JavaScript/arrays.js
@@ -0,0 +1,14 @@
+/* 
+
+Los arrays 
+
+*/
+
+var nuevoArray = [1,2,3,4];
+var nuevoArray2= ["a","b","c","d"];
+var nuevoArray3= [1+2,8*5,"hola",null]
+var nuevoArray4= [function saludar(){console.log("holiss");},"chau"];
+
+console.log(nuevoArray,nuevoArray2,nuevoArray3,nuevoArray4);
+
+console.log(nuevoArray4[0]);
