6724c6b81358cfd921c330b8df9090f3d1a3cbab
SoloWolf12 <danielaguerreberry@live.com.ar>
2021-10-20 22:03:45 -0300

    desafio 4, comp y pre entrega

--- C/borrador.c
+++ C/borrador.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-    int base=5;
-    int potencia=3;
-    int res=1;
-    
-    for (int i=0; i<potencia;i++)
+    int base = 5;
+    int potencia = 3;
+    int res = 1;
+
+    for (int i = 0; i < potencia; i++)
     {
-        res=res*base;
-        printf("%d\n",res);
+        res = res * base;
+        printf("%d\n", res);
     }
 }
